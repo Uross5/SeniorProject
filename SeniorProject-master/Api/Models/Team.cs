0987c7f67ea627387467cd977cb4ea0d117f83f3
ReaperScythe13 <breonono13853@gmail.com>
2026-04-28 09:29:39 -0400

    Remove SeniorProject master

--- SeniorProject-master/Api/Models/Team.cs
+++
@@ -1,8 +0,0 @@
-namespace Agppa.Api.Models;
-
-public class Team
-{
-    public int TeamId { get; set; }
-    public string TeamName { get; set; } = "";
-    public string? CoachName { get; set; }
-}
--- SeniorProject-master/README.md
+++
@@ -1,4 +0,0 @@
-# Basketball database / AGPPA
-
-MySQL schema in `athletedb.sql`. Web API and UI in `Api/` (open `Agppa.sln`).
-Connection string: `Api/appsettings.json`.
